45df10a2530fbe808a9aec8a77851f5b9aae413e
neeraj <me.neeraj1455@gmail.com>
2022-11-22 11:54:54 +0530

    use prop-types in app

---
+++ block-BRaaaq/my-app/src/components/Footer.js
@@ -0,0 +1,26 @@
+/** @format */
+
+function Footer(props) {
+  return (
+    <>
+      <footer>
+        <div>
+          <small>&copy all right reserved</small>
+        </div>
+        <div className="navBox">
+          <div className="footerNav">
+            <i className="fa-brands fa-facebook "></i>
+          </div>
+          <div className="footerNav">
+            <i className="fa-brands fa-twitter"></i>
+          </div>
+          <div className="footerNav">
+            <i className="fa-brands fa-instagram"></i>
+          </div>
+        </div>
+      </footer>
+    </>
+  );
+}
+
+export default Footer;
